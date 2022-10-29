fef51e4e872014c86a68cbf54bd283cad7f4d79c
Christian <christian20599@gmail.com>
2022-10-29 19:24:54 -0400

    nuevo home

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
